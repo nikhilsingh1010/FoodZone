1fd53d7e25fac768fa76b4e4c2a78d5960389b0a
nikhilsingh1010 <nikhilsinghthakur1010@gmail.com>
2024-08-26 18:18:37 +0530

    Update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import SearchResult from "./components/SearchResults/SearchResult";
 
-export const BASE_URL = "https://food-zone-server-beta.vercel.app/";
+export const BASE_URL = "http://localhost:9000";
 
 const App = () => {
   const [data, setData] = useState(null);
